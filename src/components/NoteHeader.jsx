cb9a40c6cbfc1434b550d1f41c958d97639f5850
shahryar toulabi <shahryartoolabi61@gmail.com>
2025-04-07 21:47:56 +0330

    edit

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 function NoteHeader({ notes, sortBy, onSort }) {
     return (
         <div className="note-header">
+
             <h1 className="titleHeader">my Notes ({notes.length})</h1>
+
             <select value={sortBy} onChange={onSort}>
                 <option value="latest">Sort latest</option>
                 <option value="earlieast">Sort earlieast</option>
                 <option value="cpmpleted">Sort cpmpleted</option>
             </select>
+
         </div>
 
     )
